af0a7c10c662756624c806b1a4bbd008e69303ae
Nice3point <nice3point@gmail.com>
2025-01-26 16:39:18 +0300

    Add modules context menu

--- source/RevitLookup.UI.Framework/Views/Tools/ModulesDialog.xaml.cs
+++ source/RevitLookup.UI.Framework/Views/Tools/ModulesDialog.xaml.cs
@@ -18,8 +18,15 @@
 // Software - Restricted Rights) and DFAR 252.227-7013(c)(1)(ii)
 // (Rights in Technical Data and Computer Software), as applicable.
 
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using RevitLookup.Abstractions.Models.Tools;
 using RevitLookup.Abstractions.Services.Appearance;
 using RevitLookup.Abstractions.ViewModels.Tools;
+using RevitLookup.Common.Tools;
+using RevitLookup.UI.Framework.Extensions;
 using Wpf.Ui;
 
 namespace RevitLookup.UI.Framework.Views.Tools;
@@ -39,5 +46,59 @@ public sealed partial class ModulesDialog
 #if NETFRAMEWORK
         ContainerColumn.Header = "Domain";
 #endif
+    }
+
+    private void OnMouseEnter(object sender, RoutedEventArgs args)
+    {
+        var element = (FrameworkElement) sender;
+        var moduleInfo = (ModuleInfo) element.DataContext;
+        CreateRowContextMenu(moduleInfo, element);
+    }
+
+    private void CreateRowContextMenu(ModuleInfo module, FrameworkElement row)
+    {
+        var contextMenu = new ContextMenu
+        {
+            Resources = UiApplication.Current.Resources,
+            PlacementTarget = row
+        };
+
+        var copyMenu = contextMenu.AddMenuItem("CopyMenuItem")
+            .SetHeader("Copy");
+
+        copyMenu.AddMenuItem()
+            .SetHeader("Module name")
+            .SetCommand(module, moduleInfo => Clipboard.SetDataObject(moduleInfo.Name))
+            .SetShortcut(ModifierKeys.Control, Key.C);
+
+        copyMenu.AddMenuItem()
+            .SetHeader("Path to module")
+            .SetCommand(module, moduleInfo => Clipboard.SetDataObject(moduleInfo.Path))
+            .SetShortcut(ModifierKeys.Control | ModifierKeys.Shift, Key.C);
+
+        copyMenu.AddMenuItem()
+            .SetHeader("Module version")
+            .SetCommand(module, moduleInfo => Clipboard.SetDataObject(moduleInfo.Version));
+
+#if NETCOREAPP
+        copyMenu.AddMenuItem()
+            .SetHeader("AssemblyLoadContext name")
+            .SetCommand(module, moduleInfo => Clipboard.SetDataObject(moduleInfo.Container));
+#endif
+
+        var navigateMenu = contextMenu.AddMenuItem()
+            .SetHeader("Navigate");
+
+        navigateMenu.AddMenuItem()
+            .SetHeader("Module directory")
+            .SetAvailability(File.Exists(module.Path))
+            .SetCommand(module, moduleInfo => ProcessTasks.StartShell(Path.GetDirectoryName(moduleInfo.Path)!));
+
+        navigateMenu.AddMenuItem()
+            .SetHeader("Module location")
+            .SetAvailability(File.Exists(module.Path))
+            .SetCommand(module, moduleInfo => ProcessTasks.StartShell("explorer.exe", $"/select,{moduleInfo.Path}"));
+
+        row.ContextMenu = contextMenu;
     }
 }
