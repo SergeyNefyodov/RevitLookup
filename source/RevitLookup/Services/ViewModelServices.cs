f22de24e957bd75d67e45092f7235491e2450a74
Nice3point <nice3point@gmail.com>
2025-02-11 20:46:45 +0300

    IL Repack support

--- source/RevitLookup/Services/ViewModelServices.cs
+++ source/RevitLookup/Services/ViewModelServices.cs
@@ -6,7 +6,7 @@ public static class ViewModelServices
 {
     public static void RegisterViewModels(this IServiceCollection services)
     {
-        services.Scan(selector => selector.FromCallingAssembly()
+        services.Scan(selector => selector.FromAssemblyOf<RevitLookup.Application>()
             .AddClasses(filter => filter.Where(type => type.Name.EndsWith("ViewModel")))
             .AsImplementedInterfaces(type => type.Name.EndsWith("ViewModel"))
             .WithScopedLifetime());
